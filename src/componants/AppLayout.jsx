332fbdcd25e8736b2421a9f1200fc9a99244e1ea
Rohan arya <rohanarya@outlook.in>
2024-08-03 17:23:55 +0530

    weather app start

---
+++ src/componants/AppLayout.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu, theme } from "antd";
+
+const { Header, Sider, Content } = Layout;
+
+const AppLayout = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+          <Sider collapsed={ true} style={{ background: "#DFF0FF" }}>
+        <Menu
+                  mode="inline"
+                style =  {{ background: "#DFF0FF" }}
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={<MenuUnfoldOutlined />}
+            onClick={() => {}}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <div className="text-3xl bg-red-600 font-bold underline">
+            Hello world!
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default AppLayout;
